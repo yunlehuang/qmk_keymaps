50352802127ec32e2b6993a74694e0617144ca77
Larry Huang <larryhuang64@gmail.com>
2021-02-06 01:26:38 -0600

    increased max mouse speed

--- keyboards/kyria/keymaps/lhuang_kyria/config.h
+++ keyboards/kyria/keymaps/lhuang_kyria/config.h
@@ -35,12 +35,12 @@
 
 #define MOUSEKEY_DELAY              0
 #define MOUSEKEY_INTERVAL           2
-#define MOUSEKEY_MAX_SPEED          3
-#define MOUSEKEY_TIME_TO_MAX        60
+#define MOUSEKEY_MAX_SPEED          5
+#define MOUSEKEY_TIME_TO_MAX        120
 
 #define MOUSEKEY_WHEEL_DELAY        0
 #define MOUSEKEY_WHEEL_INTERVAL     250
-#define MOUSEKEY_WHEEL_MAX_SPEED    10
+#define MOUSEKEY_WHEEL_MAX_SPEED    30
 #define MOUSEKEY_WHEEL_TIME_TO_MAX  100
 
 #define COMBO_COUNT 2
