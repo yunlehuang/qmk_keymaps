c3b6553eedaf9d477ddcc86b4e719fa26fd9a1ef
Larry Huang <larryhuang64@gmail.com>
2021-02-21 21:35:49 -0600

    new keyboard, copied layout from kyria

---
+++ keyboards/naked48/keymaps/lhuang_naked48/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2018 Salicylic_acid3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Select hand configuration */
+
+//#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define USE_SERIAL
+
+#define TAPPING_TERM 140
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define PERMISSIVE_HOLD
+
+#define MOUSEKEY_DELAY              0
+#define MOUSEKEY_INTERVAL           2
+#define MOUSEKEY_MAX_SPEED          5
+#define MOUSEKEY_TIME_TO_MAX        120
+
+#define MOUSEKEY_WHEEL_DELAY        0
+#define MOUSEKEY_WHEEL_INTERVAL     250
+#define MOUSEKEY_WHEEL_MAX_SPEED    30
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  100
+
+#define COMBO_ALLOW_ACTION_KEYS
+#define COMBO_COUNT 6
+#define COMBO_TERM 45
+
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTION 2
---
+++ keyboards/naked48/keymaps/lhuang_naked48/rules.mk
@@ -0,0 +1,6 @@
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+COMBO_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+MOUSEKEY_ENABLE = yes
