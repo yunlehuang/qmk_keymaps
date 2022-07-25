13f5607476e4496ca78cdb8a3196f28492ff9017
Larry Huang <larryhuang64@gmail.com>
2022-07-24 22:45:43 -0500

    fixed formatting for tabs. 3 tabs between spaces

--- keyboards/crkbd/keymaps/lhuang/config.h
+++ keyboards/crkbd/keymaps/lhuang/config.h
@@ -37,7 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Mousekey options
 
-#define MK_COMBINED
+// mk_combined left commented until bug is fixed in QMK.
+// #define MK_COMBINED
 
 #define MOUSEKEY_DELAY              0
 #define MOUSEKEY_INTERVAL           5     // lower is faster
