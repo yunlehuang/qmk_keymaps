3f9df81c54e1808fde52ccdb2f598812ea0bc6e6
Larry Huang <larryhuang64@gmail.com>
2022-07-25 01:29:04 -0500

    fixed mousewheel speed

--- keyboards/crkbd/keymaps/lhuang/config.h
+++ keyboards/crkbd/keymaps/lhuang/config.h
@@ -48,8 +48,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_WHEEL_DELAY        0
 #define MOUSEKEY_WHEEL_INTERVAL     100   // lower is faster
-#define MOUSEKEY_WHEEL_MAX_SPEED    100   // not really sure what this does
-#define MOUSEKEY_WHEEL_TIME_TO_MAX  0     // zero means constant
+#define MOUSEKEY_WHEEL_MAX_SPEED    4   // not really sure what this does
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  40     // zero means constant
 
 // combo settings
 
